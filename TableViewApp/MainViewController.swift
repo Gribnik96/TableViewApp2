8ffe0445ec452d96d203d628672ffb35f26799e8
Yulia <juliasheleva@gmail.com>
2021-03-24 10:44:10 +0300

    Intial

---
+++ TableViewApp/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  TableViewApp
+//
+//  Created by Юлия on 3/24/21.
+//  Copyright © 2021 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    // MARK: - Table view data source
+
+   
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+
+        // Configure the cell...
+
+        return cell
+     }
+
+    
+ 
+
+}
