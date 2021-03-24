d1da853a2c90fd7f834a5073e78c838f39e7f334
Yulia <juliasheleva@gmail.com>
2021-03-24 09:53:25 +0300

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Юлия on 3/24/21.
+//  Copyright © 2021 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
